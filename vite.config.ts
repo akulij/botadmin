b5d6509449245767bd9c53f3adddc4650b60e0d3
Akulij <aakulij@gmail.com>
2023-06-14 16:30:02 +0500

    change project structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,4 +7,13 @@ export default defineConfig({
     plugins: [
         react(),
     ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                statistics: resolve(__dirname, 'statistics/index.html'),
+                chats: resolve(__dirname, 'chats/index.html'),
+            }
+        }
+    }
 })
